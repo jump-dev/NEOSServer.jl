531dc09c9fd4d1dbb1739c68ffa845b4fb2bd2c1
odow <o.dowson@gmail.com>
2015-04-09 16:21:12 +1200

    better tests. Returns status

--- src/parser.jl
+++ src/parser.jl
@@ -1,4 +1,6 @@
 function parse_values!(m::JuMP.Model, results::String)
+	status = :UNKNOWN
+
 	if m.solver.solver == :SYMPHONY
 		# Solution Cost: 2.0000000000
 		# +++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -6,8 +8,15 @@ function parse_values!(m::JuMP.Model, results::String)
 		# +++++++++++++++++++++++++++++++++++++++++++++++++++
 		#     VAR1 2.0000000000
 		#     VAR2 1.5000000000
-		obj_reg = r"Solution Cost: ([\d.]+)"
-		var_reg = r"    VAR(\d+) ([\d.]+)"
+		obj_reg = r"Solution Cost: (-?[\d.]+)"
+		var_reg = r"VAR(\d+)\W+(-?[\d.]+)"
+		if contains(results, "Optimal Solution Found") || contains(results, "Preprocessing found the optimum")
+			status = :Optimal
+		elseif contains(results, "detected unbounded problem")
+			status = :Unbounded
+		elseif contains(results, "detected infeasibility")
+			status = :Infeasible
+		end
 	elseif m.solver.solver == :CPLEX
 		# CPLEX> MIP - Integer optimal solution:  Objective =  4.5000000000e+00
 		# CPLEX> Incumbent solution
@@ -16,8 +25,15 @@ function parse_values!(m::JuMP.Model, results::String)
 		# VAR2                          4.500000
 		# VAR3                          1.000000
 		# CPLEX> 
-		obj_reg = r"Objective =  ([\d.]+)"
-		var_reg = r"VAR(\d+)\W+([\d.]+)"
+		obj_reg = r"Objective =  (-?[\d.]+)"
+		var_reg = r"VAR(\d+)\W+(-?[\d.]+)"
+		if contains(results, "optimal solution")
+			status = :Optimal
+		elseif contains(results, "unbounded")
+			status = :Unbounded
+		elseif contains(results, "infeasible")
+			status = :Infeasible
+		end
 	elseif m.solver.solver == :XpressMP
 		# Objective function value is     4.500000
 		#	...
@@ -25,22 +41,34 @@ function parse_values!(m::JuMP.Model, results::String)
 		# C      4  VAR1      SB      1.000000      1.000000       .000000
 		# C      5  VAR2      SB      4.500000      1.000000       .000000
 		# C      6  VAR3      SB      1.000000     -1.000000       .000000
-		obj_reg = r"Objective function value is\W+([\d.]+)"
-		var_reg = r"VAR(\d+).+?([\d.]+)"
-	end
-
-	m.objVal = parsefloat(match(obj_reg, results).captures[1])
-	for v in matchall(var_reg, results)
-		regmatch = match(var_reg, v)
-		m.colVal[parseint(regmatch.captures[1])] = parsefloat(regmatch.captures[2])
-	end
-
-	# Set zero elements
-	for (i, v) in enumerate(m.colVal)
-		if isnan(v)
-			m.colVal[i] = 0.
+		obj_reg = r"Objective function value is\W+(-?[\d.]+)"
+		var_reg = r"VAR(\d+).+?(-?[\d.]+)"
+		if contains(results, "Optimal solution found")
+			status = :Optimal
+		elseif contains(results, "Problem is unbounded")
+			status = :Unbounded
+		elseif contains(results, "infeasible")
+			status = :Infeasible
 		end
 	end
 
-	return :Optimal
+	if status == :Optimal
+		m.objVal = parsefloat(match(obj_reg, results).captures[1])
+		if getObjectiveSense(m) == :Max
+			# Since MPS does not support Maximisation
+			m.objVal = -getObjectiveValue(m)
+		end
+		for v in matchall(var_reg, results)
+			regmatch = match(var_reg, v)
+			m.colVal[parseint(regmatch.captures[1])] = parsefloat(regmatch.captures[2])
+		end
+
+		# Set zero elements
+		for (i, v) in enumerate(m.colVal)
+			if isnan(v)
+				m.colVal[i] = 0.
+			end
+		end
+	end
+	return status
 end
---
+++ test/runtests.jl
@@ -0,0 +1,56 @@
+using JuMP
+using NEOS
+
+#
+# As part of the NEOS terms of use, some solvers
+# 	require a valid email to be supplied.
+#
+TESTING_EMAIL = "odow003@aucklanduni.ac.nz"
+SOLVERS = [:SYMPHONY, :CPLEX, :XpressMP]
+
+for solver in SOLVERS
+	println("\n\nTesting solver $solver\n")
+
+	m = Model(solver=NEOSSolver(solver=solver, email=TESTING_EMAIL))
+	setSolveHook(m, NEOSSolve)
+
+	@defVar(m, 0 <= x <= 1)
+	@defVar(m, 0 <= y <= 1)
+	@defVar(m, z, Bin)
+	@setObjective(m, :Min, x + y - 2z)
+	@addConstraint(m, x + y >= 5.5)
+	@addConstraint(m, x >= z)
+
+	status = solve(m)
+	@assert status == :Infeasible
+
+	m = Model(solver=NEOSSolver(solver=solver, email=TESTING_EMAIL))
+	setSolveHook(m, NEOSSolve)
+
+	@defVar(m, 0 <= x <= 1)
+	@defVar(m, y >= 0)
+	@defVar(m, z, Bin)
+	@setObjective(m, :Min, x - y - 2z)
+	@addConstraint(m, x + y >= 5.5)
+	@addConstraint(m, x >= z)
+
+	status = solve(m)
+	@assert status == :Unbounded
+
+	m = Model(solver=NEOSSolver(solver=solver, email=TESTING_EMAIL))
+	setSolveHook(m, NEOSSolve)
+
+	@defVar(m, 0 <= x <= 0.5)
+	@defVar(m, 0 <= y <= 2)
+	@defVar(m, z, Bin)
+	@setObjective(m, :Max, x - y + z)
+	@addConstraint(m, x + y >= 2)
+	@addConstraint(m, x >= z)
+
+	status = solve(m)
+	@assert status == :Optimal
+	@assert getValue(x) == 0.5
+	@assert getValue(y) == 1.5
+	@assert getValue(z) == 0.0
+	@assert getObjectiveValue(m) == -1.0
+end
