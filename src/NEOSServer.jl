bd033edc29f36a0639c6af518f76b384a6576731
Oscar Dowson <odow@users.noreply.github.com>
2023-09-13 09:29:14 +1200

    Add license header (#44)

--- src/NEOSServer.jl
+++ src/NEOSServer.jl
@@ -1,3 +1,8 @@
+# Copyright (c) 2017: Oscar Dowson and NEOS.jl contributors
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 module NEOSServer
 
 import AmplNLWriter
