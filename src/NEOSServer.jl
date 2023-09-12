5a2490cc6a2df22819a2e3651d49024c6c39bda1
Oscar Dowson <odow@users.noreply.github.com>
2023-09-13 09:42:39 +1200

    Fix license year (#45)
    I originally started working on this package in 2015

--- src/NEOSServer.jl
+++ src/NEOSServer.jl
@@ -1,4 +1,4 @@
-# Copyright (c) 2017: Oscar Dowson and NEOS.jl contributors
+# Copyright (c) 2015: Oscar Dowson and NEOS.jl contributors
 #
 # Use of this source code is governed by an MIT-style license that can be found
 # in the LICENSE.md file or at https://opensource.org/licenses/MIT.
